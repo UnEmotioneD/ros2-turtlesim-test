e01b1bfe1464080d9cb5459df33e604c068a0d75
Lemon <blackeagle10@icloud.com>
2026-01-15 20:36:49 +0900

    add TeleoprtAbsolute client to service_server

--- src/my_first_package/my_first_package/my_service_server.py
+++ src/my_first_package/my_first_package/my_service_server.py
@@ -1,6 +1,7 @@
 import rclpy as rp
 from my_first_package_msgs.srv import MultiSpawn
 from rclpy.node import Node
+from turtlesim.srv import TeleportAbsolute
 
 
 class MultiSpawning(Node):
@@ -9,13 +10,19 @@ class MultiSpawning(Node):
         self.server = self.create_service(
             MultiSpawn, "multi_spawn", self.callback_service
         )
+        self.teleport = self.create_client(
+            TeleportAbsolute, "/turtle1/teleport_absolute"
+        )
+        self.req_teleport = TeleportAbsolute.Request()
 
     def callback_service(self, request, response):
-        print(f"Request: {request}")
+        # print(f"Request: {request}")
 
-        response.x = [1.0, 2.0, 3.0]
-        response.y = [10.0, 20.0]
-        response.theta = [100.0, 200.0, 300.0]
+        # response.x = [1.0, 2.0, 3.0]
+        # response.y = [10.0, 20.0]
+        # response.theta = [100.0, 200.0, 300.0]
+        self.req_teleport.x = 1.0
+        self.teleport.call_async(self.req_teleport)
 
         return response
 
